10c53013ca88183563d2e0e5fee2c7d353654bc5
ShaneBurkhart <shaneburkhart@gmail.com>
2013-05-06 21:21:07 -0500

    Inital Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+StockEngine::Application.config.secret_token = 'a43204e32c3b7559b5e0b54571ad4ef723629da9812deb281eed59d72586c5ddbbbf406477d0f30001a13baf4ad6dc8d39538e9462f7e789296f1e42555fbeb3'
